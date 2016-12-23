faaa18f8f10f8160611be8a2f42cfdfa5760f569
Cyrille Le Clerc <cyrille@cyrilleleclerc.com>
2016-12-23 18:47:13 +0100

    Initial commit

---
+++ src/main/java/com/acmewidgets/supplychain/SupplychainPortalApplication.java
@@ -0,0 +1,12 @@
+package com.acmewidgets.supplychain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplychainPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplychainPortalApplication.class, args);
+	}
+}
