445bf7fbbd3570e461b70aed291ce664d074227d
Cyrille Le Clerc <cleclerc@cloudbees.com>
2017-07-10 10:35:15 +0200

    [SUPPLYCHAIN-456] Improve the UI

--- src/main/java/com/acmewidgets/supplychain/SupplychainPortalApplication.java
+++ src/main/java/com/acmewidgets/supplychain/SupplychainPortalApplication.java
@@ -2,11 +2,19 @@ package com.acmewidgets.supplychain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SupplychainPortalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SupplychainPortalApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String home(){
+		return "Hello world";
 	}
 }
